8efadceb4f72af1dd5f8d98ba97db06fe9a9e7ca
junderw <junderwood@bitcoinbank.co.jp>
2020-06-09 09:57:17 +0900

    More flexible benchmarks

--- bin/dontuse_bench.js
+++ bin/dontuse_bench.js
@@ -5,29 +5,49 @@ const bitcoinTS = require('bitcoin-ts');
 const { instantiateSecp256k1 } = bitcoinTS;
 const crypto = require('crypto');
 
-const ITER = 1e3;
+const RANDOM_KEY = crypto.randomBytes(32);
+RANDOM_KEY[0] &= 0x7f;
+RANDOM_KEY[31] = 0xff; // (lazy) make sure it's not invalid
 
-async function performBench(_sign, name, iter) {
-  console.log(`Staring bench for ${name} ${iter} times`);
-  const key = crypto.randomBytes(32);
-  const hash = crypto.randomBytes(32);
+const RANDOM_HASH = crypto.randomBytes(32);
+
+const PUBKEY_C = eccNative.pointFromScalar(RANDOM_KEY, true);
+const PUBKEY_UC = eccNative.pointCompress(PUBKEY_C, false);
+
+async function performBench(func, args, typeName, funcName, iter) {
+  console.log(`Starting ${iter} iteration bench for ${typeName} ${funcName}`);
   const before = Date.now();
   for(let i = 0; i < iter; i++) {
-    _sign(hash, key)
+    func(...args)
   }
   const after = Date.now();
-  console.log(`Finished ${iter} iterations of ${name} in ${after - before} ms`);
+  console.log(`Finished ${iter} iterations of       ${typeName} ${funcName} in ${after - before} ms`);
+  console.log('------------------------------------------------------------------');
 }
 
 async function main(args) {
-  const iterations = args[0] || ITER;
   const secp256k1 = await instantiateSecp256k1();
-  const secp = pkg.TinySecp.new();
-  await performBench(eccNative.sign, 'NATIVE', iterations);
-  await performBench(ecc.sign, 'JS', iterations);
-  await performBench(secp256k1.signMessageHashCompact, 'BITCOIN-TS WASM', iterations);
-  await performBench(secp.sign.bind(secp), 'OUR-WASM', iterations);
+  const secp = new pkg.TinySecp();
+  console.log('------------------------------------------------------------------');
+  for (const fixture of FIXTURES) {
+    await performBench(eccNative[fixture.name], fixture.args, 'NATIVE         ', fixture.name, fixture.iterations);
+    await performBench(ecc[fixture.name], fixture.args, 'JS             ', fixture.name, fixture.iterations);
+    await performBench(fixture.bitcoinTSEquiv(secp256k1), fixture.args, 'BITCOIN-TS WASM', fixture.name, fixture.iterations);
+    await performBench(secp[fixture.name].bind(secp), fixture.args, 'OUR-WASM       ', fixture.name, fixture.iterations);
+  }
 }
+
+const FIXTURES = [
+  {
+    name: 'sign',
+    iterations: 1000,
+    args: [
+      RANDOM_HASH,
+      RANDOM_KEY,
+    ],
+    bitcoinTSEquiv: secp256k1 => secp256k1.signMessageHashCompact,
+  },
+];
 
 main(process.argv.slice(2)).catch(err => {
   console.error(err)
